d321f0e47c3a0c33737e8abea053ed08e2ae3bc1
Çağlar <78760554+CagsDev@users.noreply.github.com>
2026-06-08 10:42:57 +0300

    Create 6_4TrafikIsigi.ino

---
+++ 6_4TrafikIsigi.ino
@@ -0,0 +1,16 @@
+const int aracKirmizi=10;
+const int aracSari=9;
+const int aracYesil=8;
+const int yayaKirmizi=7;
+const int yayaYesil=6;
+
+
+void setup() {
+  // put your setup code here, to run once:
+
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+}
